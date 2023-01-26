55d39ebec5cce01c30170c95a7e8e1ff4d8c3c57
Kimyeonho <kyh0703@nate.com>
2023-01-26 18:05:19 +0900

    [Feature] Customer Error handling

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../../errors/request-validation-error';
+import { DatabaseConnectionError } from '../../errors/database-connection-error';
+
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    return res.status(err.statusCode).send({ error: err.serializeErrors() });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
   res.status(400).send({
-    message: 'Something went wrong',
+    errors: [{ message: 'something went event' }],
   });
 };
