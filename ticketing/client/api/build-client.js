992b0175d412bbb6bc2adef600f7ff95155bff2d
Kimyeonho <kyh0703@nate.com>
2023-01-30 22:07:02 +0900

    [Feature] Complete client

--- ticketing/client/api/build-client.js
+++ ticketing/client/api/build-client.js
@@ -3,12 +3,9 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
-    console.log(req.headers);
     return axios.create({
-      //   baseURL:
-      //     'https://ingress-nginx-controller-admission.ingress-nginx.svc.cluster.local',
-      baseURL: 'http://auth-srv.kyh.svc.cluster.local:3000',
-      headers: req.headers,
+      baseURL:
+        'https://ingress-nginx-controller-admission.ingress-nginx.svc.cluster.local',
     });
   } else {
     // we must be on the browser
