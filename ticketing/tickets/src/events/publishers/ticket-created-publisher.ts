0615278129f051645a8871b9cf7aaf82b2345fab
Kimyeonho <kyh0703@nate.com>
2023-02-01 10:30:48 +0900

    [Feature] Complete

---
+++ ticketing/tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketCreatedEvent } from '@kyh-tickets/common';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
